dfa3972020ea73242c39e4aa006ffb3cf6acbcd1
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-03-21 17:59:29 -0300

    feat: use front sprite

--- src/components/PokemonCards/PokemonCard.js
+++ src/components/PokemonCards/PokemonCard.js
@@ -4,7 +4,7 @@ import './PokemonCard.css'
 const PokemonCard = ({ pokecards }) => (
     <div className="pokemon-card">
         <h1 className="pokemon-card__titulo">{pokecards.name}</h1>
-        <h2 className="pokemon-card__tipo"> </h2>
+        <h2 className="pokemon-card__tipo">{pokecards.sprites?.front_default}</h2>
         <img className="pokemon-card__imagem" alt={""} src={""} ></img>
         <div className="pokemon-card__descricao">Sua longa cauda serve como base para se proteger de sua própria energia de alta tensão.</div>
         <button className="pokemon-card__botao">Capturar!</button>
