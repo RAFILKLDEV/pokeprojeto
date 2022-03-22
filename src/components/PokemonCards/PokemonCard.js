4f5f3c36a2df9ae3b90ac89348d69d5f1a266602
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-03-22 17:13:56 -0300

    feat: use image,types and id on Pokecards

--- src/components/PokemonCards/PokemonCard.js
+++ src/components/PokemonCards/PokemonCard.js
@@ -1,13 +1,15 @@
 import React from "react";
-import './PokemonCard.css'
+import './PokemonCard.scss'
 
 const PokemonCard = ({ pokecards }) => (
     <div className="pokemon-card">
-        <h1 className="pokemon-card__titulo">{pokecards.name}</h1>
-        <h2 className="pokemon-card__tipo">{pokecards.sprites?.front_default}</h2>
-        <img className="pokemon-card__imagem" alt={""} src={""} ></img>
-        <div className="pokemon-card__descricao">Sua longa cauda serve como base para se proteger de sua própria energia de alta tensão.</div>
-        <button className="pokemon-card__botao">Capturar!</button>
+        <img alt={pokecards.name} src={pokecards.sprites?.front_default} ></img>
+        <div className="id">#{pokecards.id}</div>
+        <div className="title">{pokecards.name}</div>
+        <div className="types">
+            { pokecards.types?.map( (valor, it) => <div key={pokecards.name + it}>{ valor?.type.name }</div>) }
+        </div>
+        <button>Capturar!</button>
     </div>
 )
 
